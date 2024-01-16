ee72b7fd2efe3d11b53f5bb6d992a489436e4f39
Maximus Österman <maximus@oesterman.com>
2024-01-16 23:22:26 +0100

    done with lab1

--- lab1/convert.py
+++ lab1/convert.py
@@ -1,23 +1,65 @@
 
 # Convertion functions
 def celsius_to_fahrenheit(t_celsius):
-    return ((9/5) * t_celsius + 32) 
+    result = ((9/5) * t_celsius + 32) 
+    print(f"That corrensponds to {result} Fahrenheit")
 
 def fahrenheit_to_celsius(t_fahrenheit):
-    return (5/9) * (t_fahrenheit - 32)
+    result = (5/9) * (t_fahrenheit - 32)
+    print(f"That corrensponds to {result} Celsius")
 
 def celsius_to_kelvin(t_celsius):
-    return t_celsius + 273.15
+    result = t_celsius + 273.15
+    print(f"That corrensponds to {result} Kelvin")
 
 def kelvin_to_celsius(t_kelvin):
-    return t_kelvin - 273.15
+    result = t_kelvin - 273.15
+    print(f"That corrensponds to {result} Celsius")
 
 def fahrenheit_to_kelvin(t_fahrenheit):
-    return (((t_fahrenheit - 32)*5)/9)+273.15
+    result = (((t_fahrenheit - 32)*5)/9)+273.15
+    print(f"That corrensponds to {result} Kelvin")
 
 def kelvin_to_fahrenheit(t_kelvin):
-    return (((t_kelvin - 273.15) * 9) / 5) + 32            
+    result = (((t_kelvin - 273.15) * 9) / 5) + 32            
+    print(f"That corrensponds to {result} Fahrenheit")
+
+
+def user_input():
+    choice = float(input("Wich converstion do you want to do? "))
+
+    if choice == 1:
+        user_temprature = float(input("Give a temperature in Celsius: "))
+        celsius_to_fahrenheit(user_temprature)
+
+    if choice == 2:
+        user_temprature = float(input("Give a temperature in Fahrenheit: "))
+        fahrenheit_to_celsius(user_temprature)
+
+    if choice == 3:
+        user_temprature = float(input("Give a temperature in Celsius: "))
+        celsius_to_kelvin(user_temprature)
+    
+    if choice == 4:
+        user_temprature = float(input("Give a temperature in Kelvin: "))
+        kelvin_to_celsius(user_temprature)
+
+    if choice == 5:
+        user_temprature = float(input("Give a temperature in Fahrenheit: "))
+        fahrenheit_to_kelvin(user_temprature)
+
+    if choice == 6:
+        user_temprature = float(input("Give a temperature in Kelvin: "))
+        kelvin_to_fahrenheit(user_temprature)
+
 
 if __name__ == '__main__':
     print("Temperature converter")
+    print("1. Celsius to Fahrenheit")
+    print("2. Fahrenheit to Celsius")
+    print("3. Celsius to Kelvin")
+    print("4. Kelvin to Celsius")
+    print("5. Fahrenheit to Kelvin")
+    print("6. Kelvin to Fahrenheit\n\n")
+    user_input()
     
