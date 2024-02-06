30a203d2fcd719a16e201fe187d7863f0f0da88e
Maximus Österman <maximus@oesterman.com>
2024-02-06 10:19:34 +0100

    Task 2 done with basic file handling

--- lab3/lab3.py
+++ lab3/lab3.py
@@ -1,7 +1,7 @@
 
 def insert_in_sorted(x: int, sorted_list: list):
     for i in range(len(sorted_list)):
-        if sorted_list[i] >= x:
+        if sorted_list[i] > x:
             sorted_list.insert(i, x)
             break
 
@@ -10,8 +10,30 @@ def insert_in_sorted(x: int, sorted_list: list):
 
     return sorted_list
 
+
+def insertion_sort(my_list: list):
+    out: list = []
+    for x in my_list:
+        insert_in_sorted(x, out)
+
+    return out
+
+def number_lines(f: str):
+    try:
+        with open(f, "r") as file:
+            try:
+                with open(f"numbered_{f}", "w") as new_file:
+                    for index, line in enumerate(file):
+                        new_file.write(f"{index} {line}")
+
+            except IOError:
+                print("Error creating new file")
+
+    except IOError:
+        print("Error opening " + f)
+
 def main():
-    print(insert_in_sorted(2,[2,2]))
+    number_lines("poem.txt")
 
 if __name__ == '__main__':
     main()
